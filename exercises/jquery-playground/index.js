ae9abc143358f2c5905f1e7a212979437394b0a5
Nora Altwaijri <noraaltwaijri@gmail.com>
2019-03-05 18:54:19 +0300

    Homework Week6
    Sorry I solved the wrong one...

--- exercises/jquery-playground/index.js
+++ exercises/jquery-playground/index.js
@@ -8,48 +8,82 @@
  * When, button 1 is clicked, select all paragraphs on the page with jQuery,
  * and changed the color to blue.
  */
+ $("#button1").click(
+   function(){
+     $('p').css("color","blue");
+   }
+ );
 
 /**
 * Question 2:
 * When button 1 is clicked, select all <h2> elements on this page,
 * and change the text to say your name.
 */
-
+$("#button2").click(
+  function(){
+    $('h2').html('NORA');
+  }
+);
 /**
 * Question 3:
 * When button 1 is clicked, select the ordered list item that has the class "falseFact".
 * Using jQuery change the content to read "True Fact".
 */
-
+$("#button1").click(
+  function(){
+    $('li.falseFact').html('True Fact');
+  }
+);
 
 /**
 * Question 4:
 * When button 2 is clicked, change the background color of the whole page to "pink"
 */
-
+$("#button2").click(
+  function(){
+    $('body').css('background','pink');
+  }
+);
 /**
 * Question 5:
 * When button 2 is clicked, change the color of all h2's to "green"
 */
-
+$("#button2").click(
+  function(){
+    $('h2').css('color','green');
+  }
+);
 /**
 * Question 6:
 * When button 2 is clicked, change the html content of all "blockquotes"
 * to be "<span>no quote</span>"
 */
-
+$("#button2").click(
+  function(){
+    $('blockquotes').html('<span>no quote</span>');
+  }
+);
 /**
 * Question 7:
 * When button 3 is clicked, change the text in the header's h1 tag to read "jQuery Ninja"
 */
-
+$("#button3").click(
+  function(){
+    $('h1').html('jQuery Ninja');
+  }
+);
 /**
 * Question 8:
 * Have the following code execute when button 3 is clicked
 * Write a JavaScript comment with a description of what happened
 * $(".city").attr("src", "http://lorempixel.com/g/500/400/food");
 */
-
+$("#button3").click(
+  function(){
+    $(".city").attr("src", "http://lorempixel.com/g/500/400/food");
+    //It changed the image source to a different one.
+  }
+);
 /**
 * Question 9:
 * When button 3 is clicked, select all paragraphs on the page
@@ -57,3 +91,8 @@
 * Bonus: Try to do this with only one jQuery call.
 * $("selector").css({attribute: "value", attribute: "value"});
 */
+$("#button3").click(
+  function(){
+    $('p').css({color: 'blue', font:'Georgia'});
+  }
+);
--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,17 @@
 // click event for gray button
-
+$("#grayButton").on("click",switchGray);
 // click event for white button
-
+$("#whiteButton").on("click",switchWhite);
 function switchGray() {
     // change background to gray
+    $('body').css('background','gray');
     // change font to white
+    $('body').css('color','white');
 }
 
 function switchWhite() {
     // change background to white
+    $('body').css('background','white');
     // change font to black
+    $('body').css('color','black');
 }
